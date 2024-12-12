d4c2cb2df9892f1862569fc6d517ab433600a92c
GATAKOLA <breghinv@gmail.com>
2024-12-12 19:47:30 +0200

    Added Lab6

---
+++ Lab6/Lab6-Пример_с_2_файлами/f.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+/*
+1 задание
+
+int hello(int x);
+*/
+
+/*
+2 задание
+
+int hello(int x)
+{
+    return x * 2;
+}
+*/
+
+static int hello(int x) 
+{
+    return x * 2;
+}
---
+++ Lab6/Lab6-Простой_Пример/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+/*
+int hello(int x);
+
+int main() 
+{
+    int result = hello(5);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
+
+Если скомпилировать это то даст ошибку: undefined reference to `hello(int)'
+Это означает что компилятор не знает что это за функция, поскольку нет её определения.
+*/
+
+int hello(int x)
+{
+    return x * 2;
+}
+
+int main()
+{
+    int result = hello(5);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
